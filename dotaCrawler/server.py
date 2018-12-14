ab9cabd96a6a65c71645068bf525573445097ee6
Aaditya Chapagain <chapagain.aaditya@gmail.com>
2018-12-14 11:46:27 +0545

    attemting to enable CORS policy

--- dotaCrawler/server.py
+++ dotaCrawler/server.py
@@ -7,6 +7,7 @@ app = connexion.App(__name__,specification_dir='./')
 # Read the swagger.yml file to configure the endpoints
 app.add_api('swagger.yml')
 
+
 # Create a URL route in our application
 @app.route('/')
 def home():
